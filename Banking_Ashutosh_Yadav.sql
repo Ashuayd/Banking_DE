ad9addae1aa0cef32fef99a5fe418d633419a8e5
Ashu <ashuayd@gmail.com>
2025-04-15 15:05:52 +0530

    SQL Create Database and Tables

---
+++ Banking_Ashutosh_Yadav.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE Bash_db;
+
+CREATE TABLE users (
+	user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(20) UNIQUE NOT NULL,
+    password VARCHAR(64) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    address VARCHAR(200) NOT NULL,
+    aadhaar VARCHAR(12) NOT NULL,
+    mobile VARCHAR(10) NOT NULL,
+    account_number VARCHAR(10) UNIQUE NOT NULL,
+    balance DECIMAL(10, 2) DEFAULT 100.00
+);
+ 
+CREATE TABLE cards (
+	card_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    card_number VARCHAR(16) UNIQUE NOT NULL,
+    card_type ENUM('Debit','Credit') NOT NULL,
+    pin VARCHAR(4) NOT NULL,
+    cvv VARCHAR(3) NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE transactions (
+	transaction_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    beneficiary_account VARCHAR(10) NOT NULL,
+    transaction_date DATETIME NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE beneficiaries (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    account_number VARCHAR(10) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
